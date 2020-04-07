208ec5aa34ead14dca8da977dee292551b49ec03
Deo-Mac <druplall@gmail.com>
2020-04-07 09:45:28 -0400

    Implemented route and navaigation for product details

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   //selector: "pm-product-detail",
@@ -11,7 +12,24 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail Page";
   product: IProduct;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get("id");
+    // the id is a string value so adding the + will convert the string to a number.
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      productId: id,
+      productName: "Leaf Rake",
+      productCode: "GDN-001",
+      releaseDate: "March 19, 2019",
+      description: "Leaf rake with 48-inch wooden handle.",
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: "assets/images/leaf_rake.png",
+    };
+  }
+  onBack(): void {
+    this.router.navigate(["/products"]);
+  }
 }
